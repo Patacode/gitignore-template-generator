031dcbdf281e571997c43c81bd9a97a6e54eb60e
Patacode <pata.codegineer@gmail.com>
2025-04-09 23:07:55 +0200

    format: reformat main.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use std::{env, process::exit};
 
 fn get_call_to_gitignore_template_service(values: &String) -> String {
-    let url = format!("https://www.toptal.com/developers/gitignore/api/{values}");
+    let url =
+        format!("https://www.toptal.com/developers/gitignore/api/{values}");
 
-    ureq::get(url).call().unwrap_or_else(|error| {
-        eprintln!("An error occurred during the API call: {error}");
-        exit(2);
-    }).body_mut().read_to_string().unwrap_or_else(|_| {
-        eprintln!("An error occurred during body parsing");
-        exit(3);
-    })
+    ureq::get(url)
+        .call()
+        .unwrap_or_else(|error| {
+            eprintln!("An error occurred during the API call: {error}");
+            exit(2);
+        })
+        .body_mut()
+        .read_to_string()
+        .unwrap_or_else(|_| {
+            eprintln!("An error occurred during body parsing");
+            exit(3);
+        })
 }
 
 fn parse_vec_to_comma_separated_list(values: &[String]) -> String {
@@ -20,7 +26,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        eprintln!("You must give at least 1 CLI arg but you gave {}", args.len() - 1);
+        eprintln!(
+            "You must give at least 1 CLI arg but you gave {}",
+            args.len() - 1
+        );
         exit(1);
     }
 
