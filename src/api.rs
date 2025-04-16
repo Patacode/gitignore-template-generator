6b7ad9135bbb644ffb51f1ebb9ce535cd8d651ba
Patacode <pata.codegineer@gmail.com>
2025-04-17 01:28:43 +0200

    feat: add HttpClient trait with ureq and mock implementations
    To more easily unit tests calling function

--- src/api.rs
+++ src/api.rs
@@ -3,6 +3,10 @@ pub mod validator;
 
 use crate::impls as api_impl;
 
+pub trait HttpClient {
+    fn get(&self, url: &str) -> Result<String, String>;
+}
+
 pub fn get_call_to_gitignore_template_service(values: &String) -> String {
     api_impl::get_call_to_gitignore_template_service(values)
 }
--- src/impls.rs
+++ src/impls.rs
@@ -1,5 +1,36 @@
 use std::process::exit;
 
+use crate::api::HttpClient;
+
+pub struct UreqClient;
+pub struct MockClient {
+    response: Result<String, String>,
+}
+
+impl HttpClient for UreqClient {
+    fn get(&self, url: &str) -> Result<String, String> {
+        let result = ureq::get(url).call();
+
+        match result {
+            Ok(mut response) => match response.body_mut().read_to_string() {
+                Ok(body) => Ok(body),
+                Err(_error) => {
+                    Err(String::from("An error occurred during body parsing"))
+                }
+            },
+            Err(error) => {
+                Err(format!("An error occurred during the API call: {error}"))
+            }
+        }
+    }
+}
+
+impl HttpClient for MockClient {
+    fn get(&self, _url: &str) -> Result<String, String> {
+        self.response.clone()
+    }
+}
+
 pub fn get_call_to_gitignore_template_service(values: &String) -> String {
     let url =
         format!("https://www.toptal.com/developers/gitignore/api/{values}");
