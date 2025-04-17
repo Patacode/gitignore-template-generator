0efd0c37a18e5865fe6bc2c903a8e7a4ef55d0fd
Patacode <pata.codegineer@gmail.com>
2025-04-17 03:17:12 +0200

    test: add first unit tests for validator module

--- src/validator/api.rs
+++ src/validator/api.rs
@@ -3,3 +3,26 @@ pub use crate::validator::impls::DefaultCliArgsValidator;
 pub trait CliArgsValidator {
     fn has_no_commas(value: &str) -> Result<String, String>;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_checks_that_value_without_commas_has_no_commas() {
+        let value = "rust";
+        let expected: Result<String, String> = Ok(String::from(value));
+        let actual = DefaultCliArgsValidator::has_no_commas(value);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn it_checks_that_value_with_commas_has_no_commas() {
+        let value = "r,ust";
+        let expected: Result<String, String> = Err(String::from("Commas are not allowed in template names"));
+        let actual = DefaultCliArgsValidator::has_no_commas(value);
+
+        assert_eq!(actual, expected);
+    }
+}
