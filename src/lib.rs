db02a8e66962986cdb7a5612d25441df4a0ea0a4
Patacode <pata.codegineer@gmail.com>
2025-04-17 03:07:08 +0200

    format: reformat with cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-mod core; // core api
 mod config; // cli args configs
+mod core; // core api
 mod http_client; // http client to make api calls
 mod validator; // cli args validators
 
 // exposed entities
-pub use core::get_call_to_gitignore_template_service;
 pub use config::Args;
+pub use core::get_call_to_gitignore_template_service;
