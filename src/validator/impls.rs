ab7cfdac54b4ade1a1658021b175d3136e326837
Patacode <pata.codegineer@gmail.com>
2025-04-17 03:18:18 +0200

    fix: update error message returned when commas in value

--- src/validator/impls.rs
+++ src/validator/impls.rs
@@ -5,7 +5,7 @@ pub struct DefaultCliArgsValidator;
 impl CliArgsValidator for DefaultCliArgsValidator {
     fn has_no_commas(value: &str) -> Result<String, String> {
         if value.contains(',') {
-            Err(String::from("Commas are not allowed in file names."))
+            Err(String::from("Commas are not allowed in template names"))
         } else {
             Ok(value.to_string())
         }
