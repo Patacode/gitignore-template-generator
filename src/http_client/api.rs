e7b0fab6b91c7058050f160a06816e9563a7029d
Patacode <pata.codegineer@gmail.com>
2025-04-17 03:59:04 +0200

    test: add first unit test for http_client module

--- src/http_client/api.rs
+++ src/http_client/api.rs
@@ -1,6 +1,6 @@
 pub use crate::http_client::impls::UreqClient;
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct ProgramError {
     pub message: String,
     pub exit_status: i32,
@@ -8,4 +8,29 @@ pub struct ProgramError {
 
 pub trait HttpClient {
     fn get(&self, url: &str) -> Result<String, ProgramError>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito::Server;
+
+    #[test]
+    fn it_fetches_data_correctly_with_ureq_client() {
+        let api_response = "gitignore template for rust";
+        let mut server = Server::new();
+        let base_url = server.url();
+        let uri = "/api/rust";
+        let mock = server.mock("GET", uri)
+            .with_status(200)
+            .with_body(api_response)
+            .create();
+
+        let client = UreqClient {};
+        let expected: Result<String, ProgramError> = Ok(String::from(api_response));
+        let actual = client.get(&format!("{base_url}{uri}"));
+
+        mock.assert();
+        assert_eq!(actual, expected);
+    }
 }
