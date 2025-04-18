3b98bd557baf1a11967f8cf5bf8ab415af249a96
Patacode <pata.codegineer@gmail.com>
2025-04-18 04:15:40 +0200

    test: add more unit tests on UreqClient

--- src/http_client/api.rs
+++ src/http_client/api.rs
@@ -21,13 +21,65 @@ mod tests {
         let mut server = Server::new();
         let base_url = server.url();
         let uri = "/api/rust";
-        let mock = server.mock("GET", uri)
+
+        let mock = server
+            .mock("GET", uri)
             .with_status(200)
             .with_body(api_response)
             .create();
 
         let client = UreqClient {};
-        let expected: Result<String, ProgramError> = Ok(String::from(api_response));
+        let expected: Result<String, ProgramError> =
+            Ok(String::from(api_response));
+        let actual = client.get(&format!("{base_url}{uri}"));
+
+        mock.assert();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn it_gracefully_fails_if_non_200_response_with_ureq_client() {
+        let api_response = "error response";
+        let mut server = Server::new();
+        let base_url = server.url();
+        let uri = "/api/rust";
+
+        let mock = server
+            .mock("GET", uri)
+            .with_status(400)
+            .with_body(api_response)
+            .create();
+
+        let client = UreqClient {};
+        let expected: Result<String, ProgramError> = Err(ProgramError {
+            message: String::from(
+                "An error occurred during the API call: http status: 400",
+            ),
+            exit_status: 2,
+        });
+        let actual = client.get(&format!("{base_url}{uri}"));
+
+        mock.assert();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn it_gracefully_fails_if_body_parsing_issue_with_ureq_client() {
+        let mut server = Server::new();
+        let base_url = server.url();
+        let uri = "/api/rust";
+
+        let mock = server
+            .mock("GET", uri)
+            .with_status(200)
+            .with_body(vec![0, 159, 146, 150])
+            .create();
+
+        let client = UreqClient {};
+        let expected: Result<String, ProgramError> = Err(ProgramError {
+            message: String::from("An error occurred during body parsing"),
+            exit_status: 3,
+        });
         let actual = client.get(&format!("{base_url}{uri}"));
 
         mock.assert();
