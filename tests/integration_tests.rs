6ef1688bc88299a9ad65235579b8444db77d8c46
Patacode <pata.codegineer@gmail.com>
2025-04-18 16:39:18 +0200

    test: add integration test for no pos args given

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -31,3 +31,17 @@ fn it_outputs_correct_combined_templates_from_api() {
     assert!(output.status.success());
     assert_eq!(actual, expected);
 }
+
+#[test]
+fn it_outputs_correct_error_when_no_pos_args_provided() {
+    let mut cmd = get_test_bin("gitignore-template-generator");
+
+    let output = cmd.output().expect("Failed to execute command");
+    let expected = fs::read_to_string("tests/expected/no_pos_args_error.txt")
+        .expect("Failed to read expected output file");
+    let actual = String::from_utf8_lossy(&output.stderr);
+
+    assert!(!output.status.success());
+    assert_eq!(output.status.code(), Some(2));
+    assert_eq!(actual, expected);
+}
