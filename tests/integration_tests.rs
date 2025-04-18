1c087453c00c232563e3595abd39e4cda75f53da
Patacode <pata.codegineer@gmail.com>
2025-04-18 16:41:03 +0200

    test: add integration test on comma pos args

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -45,3 +45,19 @@ fn it_outputs_correct_error_when_no_pos_args_provided() {
     assert_eq!(output.status.code(), Some(2));
     assert_eq!(actual, expected);
 }
+
+#[test]
+fn it_outputs_correct_error_when_commas_in_pos_args() {
+    let mut cmd = get_test_bin("gitignore-template-generator");
+
+    cmd.args(["rust", "python,java"]);
+
+    let output = cmd.output().expect("Failed to execute command");
+    let expected = fs::read_to_string("tests/expected/comma_pos_args_error.txt")
+        .expect("Failed to read expected output file");
+    let actual = String::from_utf8_lossy(&output.stderr);
+
+    assert!(!output.status.success());
+    assert_eq!(output.status.code(), Some(2));
+    assert_eq!(actual, expected);
+}
