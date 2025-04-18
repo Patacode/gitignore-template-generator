d67554e44ecba6cef746b5dc65ad36cbaec2b4a6
Patacode <pata.codegineer@gmail.com>
2025-04-19 00:00:15 +0200

    refactor: group integration tests logically in module

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -3,128 +3,150 @@ use std::fs;
 use regex::Regex;
 use test_bin::get_test_bin;
 
-#[test]
-fn it_outputs_correct_single_template_from_api() {
-    let mut cmd = get_test_bin("gitignore-template-generator");
+mod happy {
+    use super::*;
 
-    cmd.arg("rust");
+    mod pos_args {
+        use super::*;
 
-    let output = cmd.output().expect("Failed to execute command");
-    let expected = fs::read_to_string("tests/expected/rust_template.txt")
-        .expect("Failed to read expected output file");
-    let actual = String::from_utf8_lossy(&output.stdout);
+        #[test]
+        fn it_outputs_template_with_one_pos_arg() {
+            let mut cmd = get_test_bin("gitignore-template-generator");
 
-    assert!(output.status.success());
-    assert_eq!(actual, expected);
+            cmd.arg("rust");
+
+            let output = cmd.output().expect("Failed to execute command");
+            let expected =
+                fs::read_to_string("tests/expected/rust_template.txt")
+                    .expect("Failed to read expected output file");
+            let actual = String::from_utf8_lossy(&output.stdout);
+
+            assert!(output.status.success());
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn it_outputs_combined_templates_with_multiple_pos_args() {
+            let mut cmd = get_test_bin("gitignore-template-generator");
+
+            cmd.args(["rust", "python"]);
+
+            let output = cmd.output().expect("Failed to execute command");
+            let expected =
+                fs::read_to_string("tests/expected/rust_python_template.txt")
+                    .expect("Failed to read expected output file");
+            let actual = String::from_utf8_lossy(&output.stdout);
+
+            assert!(output.status.success());
+            assert_eq!(actual, expected);
+        }
+    }
+
+    mod named_args {
+        use super::*;
+
+        #[test]
+        fn it_outputs_version_infos_with_version_option() {
+            let mut cmd = get_test_bin("gitignore-template-generator");
+
+            cmd.arg("-V");
+
+            let output = cmd.output().expect("Failed to execute command");
+            let pattern =
+                r"^gitignore-template-generator [0-9]+\.[0-9]+\.[0-9]+\n$";
+            let re = Regex::new(pattern).unwrap();
+            let actual = String::from_utf8_lossy(&output.stdout);
+
+            assert!(output.status.success());
+            assert!(
+                re.is_match(&actual),
+                "String did not match pattern: {}",
+                pattern
+            );
+        }
+
+        #[test]
+        fn it_outputs_author_infos_with_author_option() {
+            let mut cmd = get_test_bin("gitignore-template-generator");
+
+            cmd.arg("-a");
+
+            let output = cmd.output().expect("Failed to execute command");
+            let expected = "Patacode <pata.codegineer@gmail.com>\n";
+            let actual = String::from_utf8_lossy(&output.stdout);
+
+            assert!(output.status.success());
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn it_outputs_help_infos_with_help_option() {
+            let mut cmd = get_test_bin("gitignore-template-generator");
+
+            cmd.arg("-h");
+
+            let output = cmd.output().expect("Failed to execute command");
+            let expected =
+                fs::read_to_string("tests/expected/help_message.txt")
+                    .expect("Failed to read expected output file");
+            let expected =
+                expected.replace("{version}", env!("CARGO_PKG_VERSION"));
+            let actual = String::from_utf8_lossy(&output.stdout);
+
+            assert!(output.status.success());
+            assert_eq!(actual, expected);
+        }
+    }
 }
 
-#[test]
-fn it_outputs_correct_combined_templates_from_api() {
-    let mut cmd = get_test_bin("gitignore-template-generator");
+mod unhappy {
+    use super::*;
 
-    cmd.args(["rust", "python"]);
+    #[test]
+    fn it_outputs_error_and_fails_when_no_pos_args_provided() {
+        let mut cmd = get_test_bin("gitignore-template-generator");
 
-    let output = cmd.output().expect("Failed to execute command");
-    let expected =
-        fs::read_to_string("tests/expected/rust_python_template.txt")
-            .expect("Failed to read expected output file");
-    let actual = String::from_utf8_lossy(&output.stdout);
+        let output = cmd.output().expect("Failed to execute command");
+        let expected =
+            fs::read_to_string("tests/expected/no_pos_args_error.txt")
+                .expect("Failed to read expected output file");
+        let actual = String::from_utf8_lossy(&output.stderr);
 
-    assert!(output.status.success());
-    assert_eq!(actual, expected);
-}
+        assert!(!output.status.success());
+        assert_eq!(output.status.code(), Some(2));
+        assert_eq!(actual, expected);
+    }
 
-#[test]
-fn it_outputs_correct_error_when_no_pos_args_provided() {
-    let mut cmd = get_test_bin("gitignore-template-generator");
+    #[test]
+    fn it_outputs_error_and_fails_when_commas_in_pos_args() {
+        let mut cmd = get_test_bin("gitignore-template-generator");
 
-    let output = cmd.output().expect("Failed to execute command");
-    let expected = fs::read_to_string("tests/expected/no_pos_args_error.txt")
-        .expect("Failed to read expected output file");
-    let actual = String::from_utf8_lossy(&output.stderr);
+        cmd.args(["rust", "python,java"]);
 
-    assert!(!output.status.success());
-    assert_eq!(output.status.code(), Some(2));
-    assert_eq!(actual, expected);
-}
+        let output = cmd.output().expect("Failed to execute command");
+        let expected =
+            fs::read_to_string("tests/expected/comma_pos_args_error.txt")
+                .expect("Failed to read expected output file");
+        let actual = String::from_utf8_lossy(&output.stderr);
 
-#[test]
-fn it_outputs_correct_error_when_commas_in_pos_args() {
-    let mut cmd = get_test_bin("gitignore-template-generator");
+        assert!(!output.status.success());
+        assert_eq!(output.status.code(), Some(2));
+        assert_eq!(actual, expected);
+    }
 
-    cmd.args(["rust", "python,java"]);
+    #[test]
+    fn it_outputs_error_and_fails_when_template_not_found() {
+        let mut cmd = get_test_bin("gitignore-template-generator");
 
-    let output = cmd.output().expect("Failed to execute command");
-    let expected =
-        fs::read_to_string("tests/expected/comma_pos_args_error.txt")
-            .expect("Failed to read expected output file");
-    let actual = String::from_utf8_lossy(&output.stderr);
+        cmd.args(["foo"]);
 
-    assert!(!output.status.success());
-    assert_eq!(output.status.code(), Some(2));
-    assert_eq!(actual, expected);
-}
+        let output = cmd.output().expect("Failed to execute command");
+        let expected =
+            "An error occurred during the API call: http status: 404\n";
+        let actual = String::from_utf8_lossy(&output.stderr);
 
-#[test]
-fn it_outputs_correct_error_when_template_not_found() {
-    let mut cmd = get_test_bin("gitignore-template-generator");
-
-    cmd.args(["foo"]);
-
-    let output = cmd.output().expect("Failed to execute command");
-    let expected = "An error occurred during the API call: http status: 404\n";
-    let actual = String::from_utf8_lossy(&output.stderr);
-
-    assert!(!output.status.success());
-    assert_eq!(output.status.code(), Some(2));
-    assert_eq!(actual, expected);
-}
-
-#[test]
-fn it_outputs_correct_infos_for_version_option() {
-    let mut cmd = get_test_bin("gitignore-template-generator");
-
-    cmd.arg("-V");
-
-    let output = cmd.output().expect("Failed to execute command");
-    let pattern = r"^gitignore-template-generator [0-9]+\.[0-9]+\.[0-9]+\n$";
-    let re = Regex::new(pattern).unwrap();
-    let actual = String::from_utf8_lossy(&output.stdout);
-
-    assert!(output.status.success());
-    assert!(
-        re.is_match(&actual),
-        "String did not match pattern: {}",
-        pattern
-    );
-}
-
-#[test]
-fn it_outputs_correct_infos_for_author_option() {
-    let mut cmd = get_test_bin("gitignore-template-generator");
-
-    cmd.arg("-a");
-
-    let output = cmd.output().expect("Failed to execute command");
-    let expected = "Patacode <pata.codegineer@gmail.com>\n";
-    let actual = String::from_utf8_lossy(&output.stdout);
-
-    assert!(output.status.success());
-    assert_eq!(actual, expected);
-}
-
-#[test]
-fn it_outputs_correct_infos_for_help_option() {
-    let mut cmd = get_test_bin("gitignore-template-generator");
-
-    cmd.arg("-h");
-
-    let output = cmd.output().expect("Failed to execute command");
-    let expected = fs::read_to_string("tests/expected/help_message.txt")
-        .expect("Failed to read expected output file");
-    let expected = expected.replace("{version}", env!("CARGO_PKG_VERSION"));
-    let actual = String::from_utf8_lossy(&output.stdout);
-
-    assert!(output.status.success());
-    assert_eq!(actual, expected);
+        assert!(!output.status.success());
+        assert_eq!(output.status.code(), Some(2));
+        assert_eq!(actual, expected);
+    }
 }
