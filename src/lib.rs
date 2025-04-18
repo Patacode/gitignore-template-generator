fbeb4c79da261385c6f3a3cc5f31a9740edf8931
Patacode <pata.codegineer@gmail.com>
2025-04-18 04:30:15 +0200

    chore: make all modules public in lib and import in scope components of config and core modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod config; // cli args configs
 mod core; // core api
-mod http_client; // http client to make api calls
-mod validator; // cli args validators
+pub mod http_client; // http client to make api calls
+pub mod validator; // cli args validators
 
 // exposed entities
-pub use config::Args;
-pub use core::get_call_to_gitignore_template_service;
+pub use config::*;
+pub use core::*;
