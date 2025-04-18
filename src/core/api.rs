eca3139482671fbce114856acb717af469d4777f
Patacode <pata.codegineer@gmail.com>
2025-04-18 05:27:55 +0200

    test: add unit tests for GitignoreTemplateGenerator in core module

--- src/core/api.rs
+++ src/core/api.rs
@@ -8,3 +8,49 @@ pub trait TemplateGenerator {
         values: &str,
     ) -> Result<String, ProgramError>;
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::http_client::MockClient;
+
+    use super::*;
+
+    #[test]
+    fn it_generates_template_from_api_call_with_gitignore_generator() {
+        let client = MockClient {
+            response: Ok(String::from("all good")),
+        };
+        let url = "https://gen/api";
+        let values = "rust,python";
+
+        let expected: Result<String, ProgramError> =
+            Ok(String::from("all good"));
+        let actual = GitignoreTemplateGenerator::generate_from_api(
+            &client, url, &values,
+        );
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn it_propagates_error_if_any_with_gitignore_generator() {
+        let client = MockClient {
+            response: Err(ProgramError {
+                message: String::from("all bad"),
+                exit_status: 2,
+            }),
+        };
+        let url = "https://gen/api";
+        let values = "rust,python";
+
+        let expected: Result<String, ProgramError> = Err(ProgramError {
+            message: String::from("all bad"),
+            exit_status: 2,
+        });
+        let actual = GitignoreTemplateGenerator::generate_from_api(
+            &client, url, &values,
+        );
+
+        assert_eq!(actual, expected);
+    }
+}
