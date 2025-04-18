84f1434e8cfabfb1aa5203d17a4f8071d4844a1e
Patacode <pata.codegineer@gmail.com>
2025-04-18 16:26:18 +0200

    test: add assertion on output in happy integration test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,14 +1,18 @@
+use std::fs;
+
 use test_bin::get_test_bin;
 
 #[test]
-fn it_works() {
+fn it_outputs_correct_template_from_api() {
     let mut cmd = get_test_bin("gitignore-template-generator");
 
     cmd.arg("rust");
 
     let output = cmd.output().expect("Failed to execute command");
-
-    print!("{}", String::from_utf8_lossy(&output.stdout));
+    let expected = fs::read_to_string("tests/expected/rust_template.txt")
+        .expect("Failed to read expected output file");
+    let actual = String::from_utf8_lossy(&output.stdout);
 
     assert!(output.status.success());
+    assert_eq!(actual, expected);
 }
