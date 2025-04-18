b745dd8b15a264f46896945f7d03608ff13e63a3
Patacode <pata.codegineer@gmail.com>
2025-04-19 00:11:56 +0200

    refactor: group isolated tests logically in module

--- tests/isolation_tests.rs
+++ tests/isolation_tests.rs
@@ -1,28 +1,32 @@
 use mockito::Server;
 use test_bin::get_test_bin;
 
-#[test]
-fn it_outputs_correct_error_when_body_parsing_issue() {
-    let mut cmd = get_test_bin("gitignore-template-generator");
+mod unhappy {
+    use super::*;
 
-    let mut server = Server::new();
-    let base_url = server.url();
-    let uri = "/developers/gitignore/api/rust";
+    #[test]
+    fn it_outputs_error_and_fails_when_body_parsing_issue() {
+        let mut cmd = get_test_bin("gitignore-template-generator");
 
-    let mock = server
-        .mock("GET", uri)
-        .with_status(200)
-        .with_body(vec![0, 159, 146, 150])
-        .create();
+        let mut server = Server::new();
+        let base_url = server.url();
+        let uri = "/developers/gitignore/api/rust";
 
-    cmd.arg("rust").args(["--server-url", &base_url]);
+        let mock = server
+            .mock("GET", uri)
+            .with_status(200)
+            .with_body(vec![0, 159, 146, 150])
+            .create();
 
-    let output = cmd.output().expect("Failed to execute command");
-    let expected = "An error occurred during body parsing\n";
-    let actual = String::from_utf8_lossy(&output.stderr);
+        cmd.arg("rust").args(["--server-url", &base_url]);
 
-    mock.assert();
-    assert!(!output.status.success());
-    assert_eq!(output.status.code(), Some(3));
-    assert_eq!(actual, expected);
+        let output = cmd.output().expect("Failed to execute command");
+        let expected = "An error occurred during body parsing\n";
+        let actual = String::from_utf8_lossy(&output.stderr);
+
+        mock.assert();
+        assert!(!output.status.success());
+        assert_eq!(output.status.code(), Some(3));
+        assert_eq!(actual, expected);
+    }
 }
