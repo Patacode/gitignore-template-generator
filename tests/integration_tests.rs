4f95a7244c11c9cfea51c3e981cbf3224060a579
Patacode <pata.codegineer@gmail.com>
2025-04-18 16:30:05 +0200

    test: add integration test for multiple combined templates

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -3,13 +3,28 @@ use std::fs;
 use test_bin::get_test_bin;
 
 #[test]
-fn it_outputs_correct_template_from_api() {
+fn it_outputs_correct_single_template_from_api() {
     let mut cmd = get_test_bin("gitignore-template-generator");
 
     cmd.arg("rust");
 
     let output = cmd.output().expect("Failed to execute command");
     let expected = fs::read_to_string("tests/expected/rust_template.txt")
+        .expect("Failed to read expected output file");
+    let actual = String::from_utf8_lossy(&output.stdout);
+
+    assert!(output.status.success());
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn it_outputs_correct_combined_templates_from_api() {
+    let mut cmd = get_test_bin("gitignore-template-generator");
+
+    cmd.args(["rust", "python"]);
+
+    let output = cmd.output().expect("Failed to execute command");
+    let expected = fs::read_to_string("tests/expected/rust_python_template.txt")
         .expect("Failed to read expected output file");
     let actual = String::from_utf8_lossy(&output.stdout);
 
