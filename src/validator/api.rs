e25f0c99b6459a336573e3c6dbfba6de6d773169
Patacode <pata.codegineer@gmail.com>
2025-04-20 00:05:04 +0200

    refactor: blank line between prepare and execute section

--- src/validator/api.rs
+++ src/validator/api.rs
@@ -20,6 +20,7 @@ mod tests {
                 #[test]
                 fn it_returns_ok_for_comma_free_values() {
                     let value = "rust";
+
                     let expected: Result<String, String> =
                         Ok(String::from(value));
                     let actual = DefaultCliArgsValidator::has_no_commas(value);
@@ -36,6 +37,7 @@ mod tests {
                 #[test]
                 fn it_returns_error_for_values_with_commas() {
                     let value = "r,ust";
+
                     let expected: Result<String, String> = Err(String::from(
                         constant::error_messages::COMMAS_NOT_ALLOWED,
                     ));
