96d7e7d87ce175883f7ee6f849a4d16c97a0939a
Patacode <pata.codegineer@gmail.com>
2025-04-19 15:20:34 +0200

    style: reformat code with cargo fmt

--- src/validator/api.rs
+++ src/validator/api.rs
@@ -20,9 +20,10 @@ mod tests {
                 #[test]
                 fn it_returns_ok_for_comma_free_values() {
                     let value = "rust";
-                    let expected: Result<String, String> = Ok(String::from(value));
+                    let expected: Result<String, String> =
+                        Ok(String::from(value));
                     let actual = DefaultCliArgsValidator::has_no_commas(value);
-        
+
                     assert_eq!(actual, expected);
                 }
             }
@@ -33,10 +34,11 @@ mod tests {
                 #[test]
                 fn it_returns_error_for_values_with_commas() {
                     let value = "r,ust";
-                    let expected: Result<String, String> =
-                        Err(String::from("Commas are not allowed in template names"));
+                    let expected: Result<String, String> = Err(String::from(
+                        "Commas are not allowed in template names",
+                    ));
                     let actual = DefaultCliArgsValidator::has_no_commas(value);
-        
+
                     assert_eq!(actual, expected);
                 }
             }
