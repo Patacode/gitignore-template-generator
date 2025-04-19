34313512076e27cfac82dc468114589ca0ed0843
Patacode <pata.codegineer@gmail.com>
2025-04-19 03:17:00 +0200

    feat: add new constant module to store globally shared constant values

---
+++ src/constant.rs
@@ -0,0 +1,3 @@
+pub mod error_messages {
+    pub const CMD_EXECUTION_FAILURE: &str = "Failed to execute command";
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod config; // cli args configs
+pub mod constant; // global constants
 mod core; // core api
 pub mod http_client; // http client to make api calls
 pub mod validator; // cli args validators
