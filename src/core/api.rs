2f514c7625092fd38cdd588ceb23c28beeaff4cd
Patacode <pata.codegineer@gmail.com>
2025-04-20 01:17:14 +0200

    refactor: restructure, rename vars and reorder statements of unit tests of core module

--- src/core/api.rs
+++ src/core/api.rs
@@ -13,42 +13,63 @@ mod tests {
     use super::*;
     use crate::http_client::MockClient;
 
-    mod generate_from_api {
+    mod gitignore_template_generator {
         use super::*;
 
-        #[test]
-        fn it_generates_gitignore_template_using_provided_client() {
-            let client = MockClient {
-                response: Ok(String::from("all good")),
-            };
-            let values = "rust,python";
+        mod generate_from_api {
+            use super::*;
 
-            let expected: Result<String, ProgramError> =
-                Ok(String::from("all good"));
-            let actual =
-                GitignoreTemplateGenerator::generate_from_api(&client, values);
+            mod success {
+                use super::*;
 
-            assert_eq!(actual, expected);
-        }
+                #[test]
+                fn it_generates_template_using_provided_client() {
+                    let template_names = "rust,python";
+                    let generated_template = "all good";
+                    let http_client = MockClient {
+                        response: Ok(String::from(generated_template)),
+                    };
 
-        #[test]
-        fn it_propagates_error_from_client_if_any() {
-            let client = MockClient {
-                response: Err(ProgramError {
-                    message: String::from("all bad"),
-                    exit_status: 2,
-                }),
-            };
-            let values = "rust,python";
+                    let actual = GitignoreTemplateGenerator::generate_from_api(
+                        &http_client,
+                        template_names,
+                    );
+                    let expected: Result<String, ProgramError> =
+                        Ok(String::from(generated_template));
 
-            let expected: Result<String, ProgramError> = Err(ProgramError {
-                message: String::from("all bad"),
-                exit_status: 2,
-            });
-            let actual =
-                GitignoreTemplateGenerator::generate_from_api(&client, values);
+                    assert_eq!(actual, expected);
+                }
+            }
 
-            assert_eq!(actual, expected);
+            mod failure {
+                use crate::constant;
+
+                use super::*;
+
+                #[test]
+                fn it_propagates_error_from_client_if_any() {
+                    let template_names = "rust,pyth";
+                    let error_message = "all bad";
+                    let http_client = MockClient {
+                        response: Err(ProgramError {
+                            message: String::from(error_message),
+                            exit_status: constant::exit_status::GENERIC,
+                        }),
+                    };
+
+                    let actual = GitignoreTemplateGenerator::generate_from_api(
+                        &http_client,
+                        template_names,
+                    );
+                    let expected: Result<String, ProgramError> =
+                        Err(ProgramError {
+                            message: String::from(error_message),
+                            exit_status: constant::exit_status::GENERIC,
+                        });
+
+                    assert_eq!(actual, expected);
+                }
+            }
         }
     }
 }
