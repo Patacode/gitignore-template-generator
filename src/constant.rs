421a45ab74125f49bbdbd8c57bf9977757abb391
Patacode <pata.codegineer@gmail.com>
2025-04-19 03:27:34 +0200

    feat: define new set of constants

--- src/constant.rs
+++ src/constant.rs
@@ -1,3 +1,9 @@
 pub mod error_messages {
     pub const CMD_EXECUTION_FAILURE: &str = "Failed to execute command";
+    pub const BODY_PARSING_ISSUE: &str = "An error occurred during body parsing";
+}
+
+pub mod exit_status {
+    pub const GENERIC: i32 = 2;
+    pub const BODY_PARSING_ISSUE: i32 = 3;
 }
