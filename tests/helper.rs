927c275340fa2d1172f0c3354d61397a9a09f101
Patacode <pata.codegineer@gmail.com>
2025-04-19 06:44:19 +0200

    refactor: remove needless helper functions

--- tests/helper.rs
+++ tests/helper.rs
@@ -13,29 +13,10 @@ pub fn load_expectation_file_as_string(expectation_file_name: &str) -> String {
         .expect(constant::error_messages::FILE_READ_TO_STRING_FAILURE)
 }
 
-pub fn get_version_infos_output_pattern() -> Regex {
-    let pattern = format!(
-        r"^{} {}\n$",
-        env!("CARGO_PKG_NAME"),
-        constant::regex::SEMVER_VERSION,
-    );
-
-    Regex::new(&pattern).unwrap()
-}
-
 pub fn parse_stdout(stdout: &[u8]) -> Cow<str> {
     String::from_utf8_lossy(stdout)
 }
 
 pub fn parse_pos_args(pos_args: &str) -> Vec<&str> {
     pos_args.split_whitespace().collect()
-}
-
-pub fn get_regex_no_match_error_message(
-    actual_output: &str,
-    expected_pattern: &Regex,
-) -> String {
-    constant::error_messages::REGEX_NO_MATCH
-        .replace("{actual}", actual_output)
-        .replace("{expected}", expected_pattern.as_str())
 }
