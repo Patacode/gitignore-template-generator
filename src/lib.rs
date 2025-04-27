ff0175c21df55baa636eb1c2e249f47158b157fb
Patacode <pata.codegineer@gmail.com>
2025-04-27 23:38:46 +0200

    refactor(lib): remove unnecessary comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 //! Components used to safely generate gitignore
 //! templates from a template generator API over HTTP.
 
-pub mod constant; // global constants
-mod core; // core api
-pub mod helper; // internal helper logic
-pub mod http_client; // http client to make api calls
-pub mod parser; // cli args parser
-pub mod validator; // cli args validators
+pub mod constant;
+mod core;
+pub mod helper;
+pub mod http_client;
+pub mod parser;
+pub mod validator;
 
 // exposed entities
 pub use core::*;
