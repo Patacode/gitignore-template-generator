4198dd8c7407cec179c00ea66541c36b4bc8bcde
Patacode <pata.codegineer@gmail.com>
2025-04-27 21:19:30 +0200

    docs(constant): add doc for exit_status module

--- src/constant.rs
+++ src/constant.rs
@@ -110,8 +110,15 @@ pub mod parser_infos {
 }
 
 pub mod exit_status {
+    //! Constants for script exit status code.
+
+    /// Exit status code for successful script execution.
     pub const SUCCESS: i32 = 0;
+
+    /// Exit status code for generic script error.
     pub const GENERIC: i32 = 2;
+
+    /// Exit status code for HTTP body parsing error.
     pub const BODY_PARSING_ISSUE: i32 = 3;
 }
 
