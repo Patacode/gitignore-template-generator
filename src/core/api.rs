ded59bc507c2e41628b1ff9365b2cc88c8941825
Patacode <pata.codegineer@gmail.com>
2025-04-27 21:57:02 +0200

    docs(core): add doc for core module

--- src/core/api.rs
+++ src/core/api.rs
@@ -1,23 +1,66 @@
+//! Define components used to wrap core logic.
+//! 
+//! As per crate definition, core logic is defined as generating
+//! gitignore templates via a template generator API over HTTP. So you will
+//! find methods to respond to that need.
+
 pub use crate::core::impls::GitignoreTemplateGenerator;
 use crate::http_client::HttpClient;
 
+/// DTO struct representing an early or abrupt program exit. 
 #[derive(Clone, PartialEq, Debug)]
 pub struct ProgramError {
+
+    /// The message linked to the program exit.
     pub message: String,
+
+    /// The exit status code to be returned by the script. 
     pub exit_status: i32,
+
+    /// The ANSI-styled message linked to the program exit.
+    /// 
+    /// Same as [`ProgramError::message`] but styled.
     pub styled_message: Option<String>,
+
+    /// The kind of program exit.
     pub error_kind: ErrorKind,
 }
 
+/// Enum for kind of program exit.
 #[derive(Clone, PartialEq, Debug)]
 pub enum ErrorKind {
+
+    /// Early program exit to print version infos.
     VersionInfos,
+
+    /// Early program exit to print help infos.
     HelpInfos,
+
+    /// Early program exit to print author infos.
     AuthorInfos,
+
+    /// Abrupt program exit due to runtime error.
     Other,
 }
 
+/// Template generator trait to generate a template via an API call made
+/// over HTTP. 
 pub trait TemplateGenerator {
+
+    /// Generates a string template matching given template names.
+    /// 
+    /// The template is generated via a GET API call made over HTTP using
+    /// the given http client.
+    /// 
+    /// # Arguments
+    /// * `http_client` - The http client to be used to make the API call.
+    /// * `template_names` - The template names to be used to generated the
+    ///     actual template
+    /// 
+    /// # Returns
+    /// 
+    /// A result containing the generated template on success, or a
+    /// [`ProgramError`] on error (e.g. 4xx, network issues...).
     fn generate_from_api(
         http_client: &impl HttpClient,
         template_names: &[String],
--- src/core/impls.rs
+++ src/core/impls.rs
@@ -2,9 +2,18 @@ use crate::{constant, http_client::HttpClient};
 
 use super::{ProgramError, TemplateGenerator};
 
+/// Generator of gitignore templates.
 pub struct GitignoreTemplateGenerator;
 
 impl TemplateGenerator for GitignoreTemplateGenerator {
+
+    /// Generates a gitignore template matching given template names.
+    /// 
+    /// * Each associated gitignore template will be merged into one big string
+    /// * A 404 error will be returned if any of the given template names isn't
+    /// supported by the underlying template generator API.
+    /// 
+    /// See [`TemplateGenerator::generate_from_api`] for more infos.
     fn generate_from_api(
         http_client: &impl HttpClient,
         template_names: &[String],
