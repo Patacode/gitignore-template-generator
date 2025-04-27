d776257d75540ba452545d04d186dfb3b8609860
Patacode <pata.codegineer@gmail.com>
2025-04-27 18:51:07 +0200

    docs(config): add doc for config module

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+//! Components used to configure cli args parsing
+
 use clap::Parser;
 
 use crate::{
@@ -5,6 +7,11 @@ use crate::{
     validator::{CliArgsValidator, DefaultCliArgsValidator},
 };
 
+/// Struct to parse and gather cli args parsing result.
+/// 
+/// It should not be used directly to parse cli args, but should be
+/// used along [`crate::parser::ArgsParser`], which wraps all the complex
+/// parsing logic.
 #[derive(Parser, Debug, PartialEq, Default)]
 #[command(version, author, long_about = None)]
 #[command(about = constant::parser_infos::ABOUT)]
@@ -20,6 +27,10 @@ Author: {author}
 ")]
 #[command(disable_help_flag = true, disable_version_flag = true)]
 pub struct Args {
+    /// A non-empty list of gitignore template names.
+    /// 
+    /// Represented by the provided positional arguments, and required
+    /// unless any of `author`, `version` or `help` options are given.
     #[arg(
         required_unless_present_any = vec!["author", "version", "help"],
         value_parser = DefaultCliArgsValidator::has_no_commas,
@@ -27,6 +38,12 @@ pub struct Args {
     )]
     pub template_names: Vec<String>,
 
+    /// The gitignore template generator service url.
+    /// 
+    /// Optional value represented by the cli option
+    /// [`constant::cli_options::SERVER_URL`] that takes a string value, and
+    /// falling back to [`constant::template_generator::BASE_URL`] if not
+    /// provided in cli args.
     #[arg(
         short = constant::cli_options::SERVER_URL.short,
         long = constant::cli_options::SERVER_URL.long,
@@ -35,6 +52,11 @@ pub struct Args {
     )]
     pub server_url: String,
 
+    /// The boolean indicator of whether to display help infos or not.
+    /// 
+    /// Optional value represented by the cli option
+    /// [`constant::cli_options::HELP`], and falling back to `false` if
+    /// not provided in cli args.
     #[arg(
         id = "help",
         short = constant::cli_options::HELP.short,
@@ -44,6 +66,11 @@ pub struct Args {
     )]
     pub show_help: bool,
 
+    /// The boolean indicator of whether to display version infos or not.
+    /// 
+    /// Optional value represented by the cli option
+    /// [`constant::cli_options::VERSION`], and falling back to `false` if
+    /// not provided in cli args.
     #[arg(
         id = "version",
         short = constant::cli_options::VERSION.short,
@@ -53,6 +80,11 @@ pub struct Args {
     )]
     pub show_version: bool,
 
+    /// The boolean indicator of whether to display author infos or not.
+    /// 
+    /// Optional value represented by the cli option
+    /// [`constant::cli_options::AUTHOR`], and falling back to `false` if
+    /// not provided in cli args.
     #[arg(
         id = "author",
         short = constant::cli_options::AUTHOR.short,
@@ -64,11 +96,33 @@ pub struct Args {
 }
 
 impl Args {
+    /// Sets new value for `template_names` field.
+    /// 
+    /// It needs to be called on struct instance and effectively mutates it.
+    /// 
+    /// # Arguments
+    /// * `template_names` - The new value to be assigned to `template_names`
+    /// field.
+    /// 
+    /// # Returns
+    /// 
+    /// The mutated borrowed instance.
     pub fn with_template_names(mut self, template_names: Vec<String>) -> Self {
         self.template_names = template_names;
         self
     }
 
+    /// Sets new value for `server_url` field.
+    /// 
+    /// It needs to be called on struct instance and effectively mutates it.
+    /// 
+    /// # Arguments
+    /// * `server_url` - The new value to be assigned to `server_url`
+    /// field.
+    /// 
+    /// # Returns
+    /// 
+    /// The mutated borrowed instance.
     pub fn with_server_url(mut self, server_url: &str) -> Self {
         self.server_url = server_url.to_string();
         self
