af17f85c848edfeabe514519a21f4c56d0c6de0f
Patacode <pata.codegineer@gmail.com>
2025-04-30 01:05:34 +0200

    perf(benches): move mock server creation out of iter function
    So create it only once, at the beginning of benchmark function

--- benches/bin_benchmark.rs
+++ benches/bin_benchmark.rs
@@ -4,18 +4,19 @@ use mockito::Server;
 use test_bin::get_test_bin;
 
 fn benchmark_function(c: &mut Criterion) {
+    let mut mock_server = Server::new();
+    let mock_server_base_url = mock_server.url();
+    let template_generator_service_uri = "/custom/rust";
+
+    mock_server
+        .mock("GET", template_generator_service_uri)
+        .with_status(200)
+        .with_body(load_expectation_file_as_string("rust_template"))
+        .create();
+
     c.bench_function(env!("CARGO_PKG_NAME"), |b| {
         b.iter(|| {
             let mut cli_tool = get_test_bin(env!("CARGO_PKG_NAME"));
-
-            let mut mock_server = Server::new();
-            let mock_server_base_url = mock_server.url();
-            let template_generator_service_uri = "/custom/rust";
-            mock_server
-                .mock("GET", template_generator_service_uri)
-                .with_status(200)
-                .with_body(load_expectation_file_as_string("rust_template"))
-                .create();
 
             cli_tool
                 .arg("rust")
