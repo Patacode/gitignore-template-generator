e81cb9e7bbffcd326013b5278cac7c21ff6f9d28
Patacode <pata.codegineer@gmail.com>
2025-05-06 21:00:59 +0200

    feat: handle list command in main binary crate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::process::exit;
 
 use gitignore_template_generator::{
-    GitignoreTemplateGenerator, TemplateGenerator,
+    GitignoreTemplateGenerator, TemplateGenerator, TemplateLister, constant,
     http_client::UreqHttpClient,
     parser::{ArgsParser, ClapArgsParser},
 };
@@ -11,22 +11,41 @@ fn main() {
     let cli_args_parser = ClapArgsParser::new();
     let parsed_cli_args = cli_args_parser.parse(cli_args);
 
-    let server_url = parsed_cli_args.server_url;
-    let endpoint_uri = parsed_cli_args.endpoint_uri;
-    let template_names = parsed_cli_args.template_names;
+    if parsed_cli_args.show_list {
+        let server_url = parsed_cli_args.server_url;
+        let endpoint_uri = constant::template_lister::URI;
 
-    let http_client = UreqHttpClient { server_url };
-    let generated_template = GitignoreTemplateGenerator::generate_from_api(
-        &http_client,
-        &endpoint_uri,
-        &template_names,
-    );
+        let http_client = UreqHttpClient { server_url };
+        let template_list = GitignoreTemplateGenerator::list_from_api(
+            &http_client,
+            &endpoint_uri,
+        );
 
-    match generated_template {
-        Ok(template) => print!("{template}"),
-        Err(error) => {
-            eprintln!("{}", error.message);
-            exit(error.exit_status);
-        }
+        match template_list {
+            Ok(result) => print!("{result}"),
+            Err(error) => {
+                eprintln!("{}", error.message);
+                exit(error.exit_status);
+            }
+        };
+    } else {
+        let server_url = parsed_cli_args.server_url;
+        let endpoint_uri = parsed_cli_args.endpoint_uri;
+        let template_names = parsed_cli_args.template_names;
+
+        let http_client = UreqHttpClient { server_url };
+        let generated_template = GitignoreTemplateGenerator::generate_from_api(
+            &http_client,
+            &endpoint_uri,
+            &template_names,
+        );
+
+        match generated_template {
+            Ok(template) => print!("{template}"),
+            Err(error) => {
+                eprintln!("{}", error.message);
+                exit(error.exit_status);
+            }
+        };
     }
 }
