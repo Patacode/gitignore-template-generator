7102ae674f9bcaef7bd7d04db09282f6f8427992
Patacode <pata.codegineer@gmail.com>
2025-05-12 00:53:04 +0200

    test(benches): add benchmarking to compare template generation with/without robust check

--- benches/bin_benchmark.rs
+++ benches/bin_benchmark.rs
@@ -14,7 +14,8 @@ fn benchmark_function(c: &mut Criterion) {
         .with_body(load_expectation_file_as_string("rust_template"))
         .create();
 
-    c.bench_function(env!("CARGO_PKG_NAME"), |b| {
+    let mut template_generation_group = c.benchmark_group("template_generation");
+    template_generation_group.bench_function("Template generation without robust check", |b| {
         b.iter(|| {
             let mut cli_tool = get_test_bin(env!("CARGO_PKG_NAME"));
 
@@ -26,6 +27,21 @@ fn benchmark_function(c: &mut Criterion) {
             cli_tool.output().unwrap();
         })
     });
+
+    template_generation_group.bench_function("Template generation with robust check", |b| {
+        b.iter(|| {
+            let mut cli_tool = get_test_bin(env!("CARGO_PKG_NAME"));
+
+            cli_tool
+                .arg("rust")
+                .args(["--server-url", &mock_server_base_url])
+                .args(["--generator-uri", "/custom"])
+                .arg("--check");
+
+            cli_tool.output().unwrap();
+        })
+    });
+    template_generation_group.finish();
 }
 
 criterion_group!(benches, benchmark_function);
