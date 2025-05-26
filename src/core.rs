f57f6cc22aeea60597ac1a5c3e5bfb582e5756bd
Patacode <pata.codegineer@gmail.com>
2025-05-26 21:57:57 +0200

    docs(core): update root module doc
    Make it shorter and more explicit

--- src/core.rs
+++ src/core.rs
@@ -1,8 +1,4 @@
-//! Define components used to wrap core logic.
-//!
-//! As per crate definition, core logic is defined as generating
-//! gitignore templates via a template generator API over HTTP. So you will
-//! find methods to respond to that need.
+//! Define core components used to manage gitignore templates.
 
 mod api;
 mod impls;
